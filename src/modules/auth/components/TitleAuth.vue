87883613c882e9b022e967bf4755deb8a69b3eb4
Chris Perez <alejandroperez499@gmail.com>
2022-01-11 22:43:27 -0500

    Add quasar and components

---
+++ src/modules/auth/components/TitleAuth.vue
@@ -0,0 +1,20 @@
+<template >
+  <q-card-section>
+    <div class="text-center q-pt-lg">
+      <div class="col text-h3">todo<span class="bg-title">.legal</span></div>
+    </div>
+  </q-card-section>
+</template>
+<script>
+export default {
+  name: 'TitleAuth',
+};
+</script>
+<style  scoped>
+.bg-title {
+  background-color: black;
+  color: rgb(95, 168, 228);
+  border-radius: 0.1em;
+  padding: 0.1em;
+}
+</style>
---
+++ src/modules/auth/layouts/AuthLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-layout>
+    <q-page-container>
+      <q-page class="flex bg-image flex-center">
+        <q-card
+          v-bind:style="$q.screen.lt.sm ? { width: '80%' } : { width: '40%' }"
+        >
+        <TitleAuth />
+        <router-view />
+        </q-card>
+      </q-page>
+    </q-page-container>
+  </q-layout>
+</template>
+<script>
+
+import TitleAuth from '@/modules/auth/components/TitleAuth.vue'
+
+export default {
+  name: 'AuthLayout',
+  components: {
+    TitleAuth
+  },
+};
+</script>
+<style></style>
